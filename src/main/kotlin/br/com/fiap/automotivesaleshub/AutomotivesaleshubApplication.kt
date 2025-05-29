3aa9a4ad8aefd3870486667f8c7491d384b2937b
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-05-29 00:50:59 -0300

    first commit

---
+++ src/main/kotlin/br/com/fiap/automotivesaleshub/AutomotivesaleshubApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.automotivesaleshub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutomotivesaleshubApplication
+
+fun main(args: Array<String>) {
+    runApplication<AutomotivesaleshubApplication>(*args)
+}
