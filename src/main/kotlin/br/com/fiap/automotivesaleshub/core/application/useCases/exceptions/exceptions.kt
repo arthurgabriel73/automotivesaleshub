3a5135fc875c1b0ce1a9459222f92717cac3dffe
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-09 18:52:42 -0300

    feat: adds default messages for ApplicationException

--- src/main/kotlin/br/com/fiap/automotivesaleshub/core/application/useCases/exceptions/exceptions.kt
+++ src/main/kotlin/br/com/fiap/automotivesaleshub/core/application/useCases/exceptions/exceptions.kt
@@ -1,11 +1,11 @@
 package br.com.fiap.automotivesaleshub.core.application.useCases.exceptions
 
 open class ApplicationException(
-    override val message: String? = null,
+    override val message: String = "An application error occurred.",
     override val cause: Throwable? = null,
 ) : Exception(message, cause)
 
 data class VehicleIsAlreadyRegisteredException(
-    override val message: String? = null,
+    override val message: String = "Vehicle is already registered.",
     override val cause: Throwable? = null,
 ) : ApplicationException(message, cause)
