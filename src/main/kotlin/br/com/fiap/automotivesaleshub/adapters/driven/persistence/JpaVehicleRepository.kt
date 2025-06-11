2ae2c88325eb8be813c9044e46842a20add6aa34
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-11 13:43:55 -0300

    feat: JpaVehicleRepository

---
+++ src/main/kotlin/br/com/fiap/automotivesaleshub/adapters/driven/persistence/JpaVehicleRepository.kt
@@ -0,0 +1,8 @@
+package br.com.fiap.automotivesaleshub.adapters.driven.persistence
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface JpaVehicleRepository : JpaRepository<VehicleEntity, UUID> {
+    fun findByPlate(plate: String): VehicleEntity?
+}
