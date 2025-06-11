4ec2cc6effc3104914f04239b6ec819abc84cec9
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-11 13:44:41 -0300

    feat: config

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,13 +22,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-database-postgresql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation(kotlin("test"))
+    runtimeOnly("org.postgresql:postgresql")
+    implementation("jakarta.inject:jakarta.inject-api:2.0.0")
 }
 
 kotlin { compilerOptions { freeCompilerArgs.addAll("-Xjsr305=strict") } }
--- src/main/kotlin/br/com/fiap/automotivesaleshub/configuration/ioc/UseCasesBeanConfiguration.kt
+++ src/main/kotlin/br/com/fiap/automotivesaleshub/configuration/ioc/UseCasesBeanConfiguration.kt
@@ -1,6 +1,5 @@
 package br.com.fiap.automotivesaleshub.configuration.ioc
 
-import br.com.fiap.automotivesaleshub.adapters.driven.persistence.InMemoryVehicleRepository
 import br.com.fiap.automotivesaleshub.core.application.ports.driven.VehicleRepository
 import br.com.fiap.automotivesaleshub.core.application.ports.driver.models.RegisterVehicleDriverPort
 import br.com.fiap.automotivesaleshub.core.application.useCases.RegisterVehicleUseCase
@@ -10,8 +9,7 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class UseCasesBeanConfiguration {
     @Bean
-    fun registerVehicleUseCase(): RegisterVehicleDriverPort {
-        val vehicleRepository: VehicleRepository = InMemoryVehicleRepository()
+    fun registerVehicleUseCase(vehicleRepository: VehicleRepository): RegisterVehicleDriverPort {
         return RegisterVehicleUseCase(vehicleRepository)
     }
 }
