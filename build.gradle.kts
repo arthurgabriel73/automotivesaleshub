ca05943e308b8c5cc93d25d9e54df454b8c3959d
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-14 14:14:25 -0300

    test: adds coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.springframework.boot") version "4.0.0-SNAPSHOT"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "2.1.0"
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
 }
 
 group = "br.com.fiap"
