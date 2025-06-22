3a000a556d5c695119a3d4d0bc621a5ef99de832
arthurgabriel73 <arthurgabriel73@gmail.com>
2025-06-22 17:41:51 -0300

    fix: removes unused exceptions

--- src/main/kotlin/br/com/fiap/automotivesaleshub/core/domain/vehicle/valueObjects/PriceCurrency.kt
+++ src/main/kotlin/br/com/fiap/automotivesaleshub/core/domain/vehicle/valueObjects/PriceCurrency.kt
@@ -1,21 +1,9 @@
 package br.com.fiap.automotivesaleshub.core.domain.vehicle.valueObjects
 
-import br.com.fiap.automotivesaleshub.core.domain.payment.exceptions.InvalidPriceCurrencyException
-
 enum class PriceCurrency {
     EUR,
     USD,
     BRL;
 
-    companion object {
-        fun fromString(priceCurrency: String): PriceCurrency {
-            return when (priceCurrency.uppercase()) {
-                "EUR" -> EUR
-                "USD" -> USD
-                "BRL" -> BRL
-                else ->
-                    throw InvalidPriceCurrencyException("Invalid price currency: $priceCurrency")
-            }
-        }
-    }
+    companion object
 }
